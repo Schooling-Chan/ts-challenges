edd3f255e481e7859a53e5239b95548f6dc6182c
jak-chan <jakchan2021@gmail.com>
2022-05-15 16:35:35 +0800

    feat: Add project to create-template script

---
+++ scripts/index.ts
@@ -0,0 +1,27 @@
+import fs from "fs";
+import path from "path";
+import { __dirname } from "./constants";
+
+const fileName = process.argv[3];
+
+const targetPath = path.resolve(__dirname, "../type-template", fileName);
+const srcPath = path.resolve(__dirname, "./template");
+
+const renderTemplate = (src: string, target: string) => {
+  const stats = fs.statSync(src);
+
+  // 递归生成文件
+  if (stats.isDirectory()) {
+    fs.mkdirSync(target, { recursive: true });
+    for (const file of fs.readdirSync(src)) {
+      renderTemplate(path.resolve(src, file), path.resolve(target, file));
+    }
+    return;
+  }
+
+  fs.copyFileSync(src, target);
+};
+
+renderTemplate(srcPath, targetPath);
+
+console.log("DONE!");
---
+++ scripts/template/template.ts
@@ -0,0 +1,1 @@
+type HelloWorld1 = string; // expected to be a string
